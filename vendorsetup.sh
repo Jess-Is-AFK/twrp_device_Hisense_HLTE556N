a15efcf211bc08380270e20faa6a515750b1c06a
Jessica Keeler <134561618+Jess-Is-AFK@users.noreply.github.com>
2024-04-22 09:46:23 -0500

    Update vendorsetup.sh
    Update omni to twrp

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_HLTE556N-user
-add_lunch_combo omni_HLTE556N-userdebug
-add_lunch_combo omni_HLTE556N-eng
+add_lunch_combo twrp_HLTE556N-user
+add_lunch_combo twrp_HLTE556N-userdebug
+add_lunch_combo twrp_HLTE556N-eng
